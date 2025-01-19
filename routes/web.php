ad9dd1cf8cc23f7cd558ae2de46f01388a768453
Hassan_Osama <87085117+hassanOsama720@users.noreply.github.com>
2025-01-19 11:34:12 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return "Hello World"
+    return "Hello World";
+});
+Route::get('/hello', function () {
+    return "Hello";
 });
